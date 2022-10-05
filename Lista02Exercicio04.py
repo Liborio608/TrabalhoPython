8f69a4ac0825ce498cdb68f61ebdef52c05683aa
Liborio608 <105176829+Liborio608@users.noreply.github.com>
2022-10-05 17:05:25 -0300

    Create Lista02Exercicio04.py

---
+++ Lista02Exercicio04.py
@@ -0,0 +1,21 @@
+id = []
+alt = []
+for i in range(1, 6):
+    print('Pessoa %d' %i)
+    idade = int(input('Sua Idade: '))
+    altura = float(input('Sua Altura: '))
+    i.append(idade)
+    alt.append(altura)
+
+print('Ordem inversa')
+print('Total de Alturas')
+print(alt[::-1])
+print('Total de Idades')
+print(id [::-1])
+
+print('Ordem Original')
+print('Total de Alturas:')
+print(alt)
+
+print('Total de Idades:')
+print(id)
