62a359328a916edfb21ee5e26c54806b63fac43c
Liborio608 <105176829+Liborio608@users.noreply.github.com>
2022-10-05 16:08:36 -0300

    Create Lista01Exercicio02.py

---
+++ Lista01Exercicio02.py
@@ -0,0 +1,10 @@
+kg = float(input("Quantos Kg você Pescou: "))
+limite = 0
+multa = 0
+if kg <= 50:
+    print("Parabéns você nao Ultrapassou o limite! ")
+else:
+    limite = kg - 50
+    multa = limite * 4
+    print("Você ultrapassou o limite de: %.2f" %limite)
+    print("Sua multa e no valor de: %.2f" %multa)
